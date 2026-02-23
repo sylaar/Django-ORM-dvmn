dc58d0fa0cb0ed5cb730a64dd3762ada4892f118
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-02-23 16:54:13 +0300

    Replaced python-dotenv with environs

--- django-orm-watching-storage-part-2/project/settings.py
+++ django-orm-watching-storage-part-2/project/settings.py
@@ -1,30 +1,30 @@
 import os
 
-from dotenv import load_dotenv
+from environs import env
 
 
-load_dotenv()
+env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'ENGINE': env('DB_ENGINE'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', 'True')
+DEBUG = env.bool('DEBUG', True)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', [])]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
